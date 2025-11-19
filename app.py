059aad96fb38541864f7e3e196f6fcd477f932cc
Leandro Alves <leandrowaalves@gmail.com>
2025-11-19 11:28:52 -0300

    nome do commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import sqlite3
+
+conn = sqlite3.connect('usuarios.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL
+)
+''')
+conn.commit()
+conn.close()
